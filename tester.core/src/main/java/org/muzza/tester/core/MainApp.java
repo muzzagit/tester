ca35a1987eb5075affd8fd0cf7863bbb46ad4ace
Muzzammil Ayyubi <muzzammil.ayyubi.tech@gmail.com>
2015-11-02 12:05:32 +0530

    initial commit, added test controller : muzza

--- tester.core/src/main/java/org/muzza/tester/core/MainApp.java
+++ tester.core/src/main/java/org/muzza/tester/core/MainApp.java
@@ -1,16 +1,22 @@
 package org.muzza.tester.core;
 
-import org.muzza.tester.web.BaseController;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Hello world!
  *
  */
+
+@Configuration
+@ComponentScan(basePackages = {"org.muzza.tester"})
+@EnableAutoConfiguration
 public class MainApp
 {
     public static void main( String[] args ) {
 
-        SpringApplication.run(BaseController.class, args);
+        SpringApplication.run(MainApp.class, args);
     }
 }
--- tester.core/src/main/java/org/muzza/tester/web/BaseController.java
+++ tester.core/src/main/java/org/muzza/tester/web/BaseController.java
@@ -1,20 +1,18 @@
 package org.muzza.tester.web;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
- * Created by muzzammil.ayyubi on 002 02 Nov, 15.
+ * Created by muzzammil.ayyubi on 02 Nov, 15.
  */
 @Controller
-@EnableAutoConfiguration
 public class BaseController {
 
     @RequestMapping("/")
     @ResponseBody
-    String home() {
+    public String home() {
         return "Testing the spring web app";
     }
 

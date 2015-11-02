1e2047b54c1f173167229959afff961143674aeb
Muzzammil Ayyubi <muzzammil.ayyubi.tech@gmail.com>
2015-11-02 13:08:40 +0530

    updated the controller to rest service controller : muzza

--- tester.core/src/main/java/org/muzza/tester/web/BaseController.java
+++ tester.core/src/main/java/org/muzza/tester/web/BaseController.java
@@ -3,16 +3,18 @@ package org.muzza.tester.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by muzzammil.ayyubi on 02 Nov, 15.
  */
-@Controller
+@RestController
+@RequestMapping("/")
 public class BaseController {
 
-    @RequestMapping("/")
+    @RequestMapping("test")
     @ResponseBody
-    public String home() {
+    public String test() {
         return "Testing the spring web app";
     }
 
